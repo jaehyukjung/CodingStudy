a40d7c0c75f45d17b715b29661870325ab3339bf
seokhwan-an <asok369258@naver.com>
2022-03-12 21:22:54 +0900

    [22.03.12][SeokHwan][Update]BOJ_16953.java

--- 1. 그리디/안석환/BOJ_16953.java
+++ 1. 그리디/안석환/BOJ_16953.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class BOJ_16953 {
@@ -12,43 +10,29 @@ public class BOJ_16953 {
         st = new StringTokenizer(br.readLine());
         int A = Integer.parseInt(st.nextToken());
         int B = Integer.parseInt(st.nextToken());
-        int[] check = new int[1000000001];
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(B);
-        boolean ok = true;
-        while(!q.isEmpty()){
-            int y = q.poll();
-            if(y % 2 == 0){
-                if(check[y/2] == 0){
-                    check[y/2] = check[y] + 1;
-                    q.offer(y/2);
-                }
+        int cnt = 0;
+        while(B > A){
+            if(B % 2 == 0){
+                cnt++;
+                B /= 2;
             }
             else{
-                String b = String.valueOf(y);
+                String b = String.valueOf(B);
                 if(b.charAt(b.length()-1) == '1'){
-                    if(b.equals("1")){
-                        check[1] = check[y] + 1;
-                        continue;
-                    }
                     b = b.substring(0,b.length()-1);
-                    if(check[Integer.parseInt(b)] == 0){
-                        check[Integer.parseInt(b)] = check[y] + 1;
-                        q.offer(Integer.parseInt(b));
-                    }
+                    cnt++;
+                    B = Integer.parseInt(b);
+                }
+                else{
+                    break;
                 }
             }
         }
         if(A == B){
-            System.out.println(0);
+            System.out.println(cnt+1);
         }
         else{
-            if(check[A] == 0){
-                System.out.println(-1);
-            }
-            else{
-                System.out.println(check[A]+1);
-            }
+            System.out.println(-1);
         }
     }
 }
