f468a73662669b42068c5b10064821ad61f14f9d
seokhwan-an <asok369258@naver.com>
2022-03-16 14:59:13 +0900

    [22.03.16][SeokHwan][ADD]BOJ_17413.java(진행중)

---
+++ 2. 구현/안석환/BOJ_17413.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_17413 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        String str = br.readLine();
+        String[] step1 = str.split("<");
+        String[] step2 = str.split(">");
+    }
+}
