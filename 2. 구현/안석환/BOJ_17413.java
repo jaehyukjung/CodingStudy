40d09ec9217b3f6b1268c8eb276463e4a8f0b788
seokhwan-an <asok369258@naver.com>
2022-03-17 17:28:26 +0900

    [22.03.17][SeokHwan][UPDATE]BOJ_17413.java

--- 2. 구현/안석환/BOJ_17413.java
+++ 2. 구현/안석환/BOJ_17413.java
@@ -6,10 +6,63 @@ import java.util.StringTokenizer;
 public class BOJ_17413 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        st = new StringTokenizer(br.readLine());
         String str = br.readLine();
-        String[] step1 = str.split("<");
-        String[] step2 = str.split(">");
+        String answer = "";
+        if(str.contains("<") || str.contains(">")){
+            boolean check = false;
+            for(int i = 0; i < str.length(); i++){
+                if(str.charAt(i) == '<'){
+                    check = true;
+                    if(!answer.equals("")){
+                        for(int j = answer.length()-1; j >= 0; j--) {
+                            System.out.print(answer.charAt(j));
+                        }
+                        answer = "";
+                    }
+                    System.out.print("<");
+                }
+                else if(str.charAt(i) == '>'){
+                    System.out.print(">");
+                    check = false;
+                }
+                else if(str.charAt(i) == ' '){
+                    if(answer.equals("")) {
+                        System.out.print(" ");
+                    }
+                    else{
+                        for(int j = answer.length()-1; j >= 0; j--){
+                            System.out.print(answer.charAt(j));
+                        }
+                        System.out.print(" ");
+                        answer="";
+                    }
+
+                }
+                else{
+                    if(check){
+                        System.out.print(str.charAt(i));
+                    }
+                    else{
+                        answer = answer + str.charAt(i);
+                    }
+                }
+            }
+            if(!answer.equals("")){
+                for(int j = answer.length()-1; j >= 0; j--) {
+                    System.out.print(answer.charAt(j));
+                }
+                answer = "";
+            }
+        }
+        else{
+            String[] next1 = str.split(" ");
+            for(int i = 0; i < next1.length; i++){
+                for(int j = next1[i].length()-1; j >= 0; j--){
+                    char answer2 = next1[i].charAt(j);
+                    System.out.print(answer2);
+                }
+                System.out.print(" ");
+            }
+        }
     }
 }
